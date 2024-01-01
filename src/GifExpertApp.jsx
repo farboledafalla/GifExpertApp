f14a2ea40ca46a865cc2e950a688b57c53e06a14
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-01-01 10:11:36 -0500

    Se agregan lineas para crear una lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,27 @@
-// 1. 
+import { useState } from 'react';
+
+// 1. Punto de entrada a la App
 export const GifExpertApp = () => {
    //ePeisepz0RLx7wGhW5tKRqHyZg2TEYUW
+
+   // 2. Categorias
+   const [categories, setCategories] = useState(['One Puch', 'Dragon Ball']);
+   console.log(categories);
    return (
       <>
+         {/* Título */}
          <h1>GifExpertApp</h1>
+
+         {/* Input */}
+
+         {/* Listado de Gifs */}
+         <ol>
+            {categories.map((category) => {
+               return <li key={category}>{category}</li>;
+            })}
+         </ol>
+
+         {/* Gif Item */}
       </>
    );
 };
