aa29f127dcf04534ecd27e9d6799c416be4da7e7
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-01-02 23:49:15 -0500

    Se agrega instruccion sin desestructurar para imprimir las imagenes en el navegador

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
+   const [images, setImages] = useState([]);
+
+   const getImages = async () => {
+      const newImages = await getGifs(category);
+      setImages(newImages);
+   };
+
    useEffect(() => {
-      getGifs(category);
+      getImages();
    }, []);
 
    return (
       <>
          <h3>{category}</h3>
+
+         <ol>
+            {images.map((img) => (
+               <li key={img.id}>
+                  <h5>{img.title}</h5>
+                  <img src={img.url} />
+               </li>
+            ))}
+         </ol>
       </>
    );
 };
