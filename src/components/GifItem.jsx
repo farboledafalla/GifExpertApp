d86fc77b7d4ba78ec80cfadc767dcc8c03d4823c
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-01-03 21:51:48 -0500

    Se agregan clases de css

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,4 +1,5 @@
 export const GifItem = ({ title, url }) => {
-   console.log(title, url);
-   return <div>GifItem</div>;
+   return <div className='card'>
+    <img src={url} alt={title} />
+   </div>;
 };
